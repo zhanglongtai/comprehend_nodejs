a425a0ad1e87895d28ab26a99cbcd37da5e42031
zhanglongtai <neo_seele@hotmail.com>
2016-03-28 22:45:24 +0800

    Update 4.3.1.js

--- Chapter_04/4.3.1.js
+++ Chapter_04/4.3.1.js
@@ -5,3 +5,22 @@ emitter.on("event1", function (message) {
 // publish
 emitter.emit('event1','I am message!');
 
+//
+var options = {
+    host: 'www.google.com',
+    port: 80,
+    path: '/upload',
+    method: 'POST'
+};
+
+var req = http.request(options, function(res) {
+    console.log('STATUS: ' + res.statuCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf-8');
+    res.on('data', function(chunk) {
+        console.log('BODY:' + chunk);
+    });
+    res.on('end', function() {
+        // TODO
+    });
+});
